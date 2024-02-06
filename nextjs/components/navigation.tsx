6bda16de1e88460b46ef240e0d298dad425c2cca
TaeYoon Kim <xodbsrla1025@naver.com>
2024-02-06 13:36:07 +0900

    02:7

--- nextjs/components/navigation.tsx
+++ nextjs/components/navigation.tsx
@@ -1,9 +1,11 @@
 'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Navigation() {
   const path = usePathname();
+
   return (
     <nav>
       <ul>
