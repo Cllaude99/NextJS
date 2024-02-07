b07b49f897476936072d8fb64a5c5ebd1629e255
TaeYoon Kim <xodbsrla1025@naver.com>
2024-02-07 13:46:51 +0900

    02:8

---
+++ nextjs/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
--- nextjs/app/about-us/page.tsx
+++ nextjs/app/about-us/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About-Us',
+};
+
 export default function AboutUs() {
   return <h1>About us!</h1>;
 }
