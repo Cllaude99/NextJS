2be542db0d2462ef8f18dc59785c8d26679425e4
TaeYoon Kim <xodbsrla1025@naver.com>
2024-02-07 22:08:44 +0900

    03:3

--- nextjs/app/(home)/page.tsx
+++ nextjs/app/(home)/page.tsx
@@ -1,13 +1,23 @@
+import axios from 'axios';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
-export default function Home() {
+const URL = `https://nomad-movies.nomadcoders.workers.dev/movies`;
+
+async function getMovies() {
+  const { data: movies } = await axios.get(URL);
+  return movies;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+
   return (
     <div>
-      <h1>hello</h1>
+      <h1>{JSON.stringify(movies)}</h1>
     </div>
   );
 }
