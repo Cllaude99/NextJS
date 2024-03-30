f0a6a1d23239ba53e210d6a318a28d198ee57cbb
Cllaude99 <xodbsrla1025@naver.com>
2024-03-30 23:57:04 +0900

    03: END

--- nextjs-14/app/(home)/page.tsx
+++ nextjs-14/app/(home)/page.tsx
@@ -1,9 +1,17 @@
+import axios from 'axios';
 import { Metadata } from 'next';
+import { API_URL } from '../constants';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
-export default function Home() {
-  return <h1>Hello!</h1>;
+async function getMovies() {
+  const { data } = await axios.get(API_URL);
+  return data;
+}
+
+export default async function Home() {
+  const data = await getMovies();
+  return <p>{JSON.stringify(data)}</p>;
 }
--- nextjs-14/app/layout.tsx
+++ nextjs-14/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import Navigation from '@/components/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
